3b6a9165c74fbba2b9be382383cb30f44d3476cf
wu1274704958 <1274704958@qq.com>
2019-12-09 14:38:31 +0800

    config

--- gyc/main.cpp
+++ gyc/main.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
 #include <boost/filesystem.hpp>
 
+#define CONFIG_FILE ".gycrc"
+
+bool conf_exist();
+
 int main()
 {
+	std::cout << std::boolalpha << conf_exist() << std::endl;
+}
 
+bool conf_exist()
+{
+	namespace fs = boost::filesystem;
+	fs::path p(CONFIG_FILE);
+	return fs::exists(p);
 }
